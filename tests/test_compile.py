00ceca6656044c434ed308dd730d5d639eef7da2
Chris Simpkins <git.simpkins@gmail.com>
2020-11-21 12:17:03 -0500

    add regular.compile function unit tests

---
+++ tests/test_compile.py
@@ -0,0 +1,14 @@
+import pytest
+
+import regular
+
+
+def test_regularexpression_compile_success():
+    # should not raise exception
+    regular.compile("[^01]")
+
+
+def test_regularexpression_compile_fail():
+    # should raise
+    with pytest.raises(Exception) as e:
+        regular.compile("\\\\\\\\\\\\\\")
